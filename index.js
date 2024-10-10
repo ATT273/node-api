3699b307359cc2d9026f47f6094d46959527f396
ATT273 <anhtuan273@gmail.com>
2024-10-10 19:27:26 +0700

    init: setup

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 import express from 'express';
-const app = express();
+import { CONNECT_DB, GET_DB } from './src/config/mongodb.js';
+import exitHook from 'async-exit-hook'
+import userRouter from './src/routes/users.js';
+import authRouter from './src/routes/auth.js';
 
-const host = 'localhost';
-const port = 5000;
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const START_SERVER = async () => {
+  // await CONNECT_DB();
+  const app = express();
 
-app.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+  const host = 'localhost';
+  const port = 5000;
+
+  // middleware
+  app.use(express.json());
+
+  app.get('/', async function (req, res) {
+    const collection = await GET_DB().listCollections().toArray();
+    console.log(collection)
+    res.send('Hello World')
+  })
+  app.use('/api/users', userRouter);
+  app.use('/api/auth', authRouter);
+  app.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+  });
+
+  exitHook((signal) => {
+    console.log('Server is shutting down...', signal)
+  })
+}
+
+CONNECT_DB()
+.then(() => console.log('Connected to Mongo database'))
+.then(() => START_SERVER())
+.catch(error => {
+  console.error('Error connecting to Mongo database', error)
+  process.exit(0)
 });
---
+++ src/controllers/authController.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const signUp = async (req, res) => { }
+
+const logIn = async (req, res) => { }
+
+const logOut = async (req, res) => { }
+
+export { signUp, logIn, logOut }
